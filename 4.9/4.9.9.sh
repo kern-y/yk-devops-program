b6d4fd5f99a86e3494ff1f10f7dda5b93d741685
kern-y <y.kernitsky@gmail.com>
2020-05-05 01:01:29 +0300

    updating empty 4.9.9 script and uploading updated pdf 4-9

--- 4.9/4.9.9.sh
+++ 4.9/4.9.9.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+date_string=`date`
+
+#deleting time zone signs
+upd_date=`echo ${date_string%+*0}`
+
+#declaring variabls as substrings of date and month
+dd=`echo ${date_string:10:1}`
+month=`echo ${date_string:12:6}`
+
+#replacing date and month
+dd_upd=`echo ${upd_date/$month/$dd}`
+month_upd=`echo ${dd_upd/$dd/$month}`
+
+#printing result
+echo $month_upd
+
